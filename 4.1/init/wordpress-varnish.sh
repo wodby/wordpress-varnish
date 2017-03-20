bd3e64ad157d32391871d4ca359b54be30e6c65d
Chingis Sandanov <chingis@wodby.com>
2017-03-20 19:08:26 +0700

    Image revamp

--- 4.1/init/wordpress-varnish.sh
+++ 4.1/init/wordpress-varnish.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ -n $DEBUG ]]; then
+if [[ -n "${DEBUG}" ]]; then
   set -x
 fi
 
@@ -12,32 +12,12 @@ function execTpl {
     fi
 }
 
-execInitScripts() {
-    shopt -s nullglob
-    for f in /docker-entrypoint-init.d/*.sh; do
-        echo "$0: running $f"
-        . "$f"
-    done
-    shopt -u nullglob
-}
-
-checkVarnishSecret() {
-    if [[ -z $VARNISH_SECRET ]]; then
-        export VARNISH_SECRET=$(pwgen -s 128 1)
-        echo "Generated Varnish secret: ${VARNISH_SECRET}"
-    fi
-}
-
-checkVarnishSecret
+mkdir -p /etc/varnish/conf
+mkdir -p /etc/varnish/lib
 
 execTpl 'varnishd.init.d.tpl' '/etc/init.d/varnishd'
 execTpl 'secret.tpl' '/etc/varnish/secret'
 execTpl 'default.vcl.tpl' '/etc/varnish/default.vcl'
-
-ls -la /etc/gotpl
-
-mkdir -p /etc/varnish/conf
-mkdir -p /etc/varnish/lib
 
 execTpl 'conf/acl.vcl.tpl'         '/etc/varnish/conf/acl.vcl'
 execTpl 'conf/backend.vcl.tpl'     '/etc/varnish/conf/backend.vcl'
@@ -48,8 +28,3 @@ execTpl 'lib/mobile_pass.vcl.tpl'  '/etc/varnish/lib/mobile_pass.vcl'
 execTpl 'lib/purge.vcl.tpl'        '/etc/varnish/lib/purge.vcl'
 execTpl 'lib/static.vcl.tpl'       '/etc/varnish/lib/static.vcl'
 execTpl 'lib/xforward.vcl.tpl'     '/etc/varnish/lib/xforward.vcl'
-
-chmod +x /etc/init.d/varnishd
-execInitScripts
-
-exec "$@"
